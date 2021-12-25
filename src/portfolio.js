ae8518ce96c624347c8e29ca60851213ddfaf32d
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-12-25 21:20:37 +0200

    Update the ting

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,7 +7,7 @@ const about = {
   name: 'Maximilian Hagelstam',
   role: 'Fullstack Developer',
   description:
-    "I'm a full-stack developer from Finland. I take pride in building fast, scalable applications using modern web technologies. Currently, I'm a high school student, but in my free time, you will find me learning more about my favourite technologies.",
+    "I'm a full-stack developer from Finland. I take pride in building fast, scalable applications using modern web technologies. Currently, I'm a high school student, but in my free time you will find me building pet projects and learning more about my favorite technologies.",
   social: {
     linkedin: 'https://www.linkedin.com/in/maximilian-hagelstam-704840186/',
     github: 'https://github.com/MaximilianHagelstam',
@@ -16,25 +16,11 @@ const about = {
 
 const projects = [
   {
-    name: 'Translatir',
-    description: 'Translation app, powered by Azure Translator.',
-    stack: ['React', 'Material UI', 'Azure Translator'],
-    sourceCode: 'https://github.com/MaximilianHagelstam/translatir',
-    livePreview: 'https://translatir.netlify.app/',
-  },
-  {
-    name: 'Wordly',
-    description: 'Word remembering app, for expanding your vocabulary.',
-    stack: ['ASP.NET Core', 'SQL Server', 'React'],
-    sourceCode: 'https://github.com/MaximilianHagelstam/Wordly',
-    livePreview: 'https://wordly.azurewebsites.net/',
-  },
-  {
-    name: 'Ocr Api',
-    description: 'An API, that parses the text in images using Tesseract.',
-    stack: ['C#', 'ASP.NET Core', 'Tesseract'],
-    sourceCode: 'https://github.com/MaximilianHagelstam/OcrApi',
-    livePreview: 'https://ocrapi.azurewebsites.net/api/home/',
+    name: '...',
+    description: 'Coming soon',
+    // stack: ['React', 'Material UI', 'Azure Translator'],
+    // sourceCode: 'https://github.com/MaximilianHagelstam/translatir',
+    // livePreview: 'https://translatir.netlify.app/',
   },
 ];
 
